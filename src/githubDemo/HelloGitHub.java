2fbc73d9b308603ef774ce4a63bc07aae37e5ffc
unknown <muhitulislamchowdhury@gmail.com>
2022-01-30 16:10:35 -0600

    01302022 4:10pm

---
+++ src/githubDemo/HelloGitHub.java
@@ -0,0 +1,9 @@
+package githubDemo;
+
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.print("I know how to create a separate remote repository in GitHub and push my project GitHubDemo");
+
+	}
+
+}
